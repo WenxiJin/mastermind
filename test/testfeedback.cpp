cfc16f1fa345af17fc9c74d166512b2c5ddfc7b7
Wenxi Jin <wenxi.jin@biolinscientific.com>
2017-03-22 14:19:06 +0100

    Include c file instead
    Including header file does not work.

--- test/testfeedback.cpp
+++ test/testfeedback.cpp
@@ -1,8 +1,13 @@
 #include "gtest/gtest.h"
-#include "feedback.h"
+#include "feedback.c"
 
 TEST(feedback_test, non_duplicate) {
-
+    EXPECT_EQ(40, feedback(1234, 1234));
+    EXPECT_EQ( 2, feedback(8912, 3689));
+    EXPECT_EQ(10, feedback(1234, 5674));
+    EXPECT_EQ(30, feedback(1235, 1234));
+    EXPECT_EQ( 4, feedback(1234, 3412));
+    EXPECT_EQ(22, feedback(1234, 1324));
 }
 
 
